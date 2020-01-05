202d35b0ce6d5ad3d7d18842e50505d8c6a55daa
bluprint1453 <luozl2015@foxmail.com>
2020-01-05 17:18:12 +0800

    add prerender demo

---
+++ prerender/js/chunk-2d0ceb84.b75c45ba.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ceb84"],{"613f":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("About page")])])}],r=t("2877"),l={},a=Object(r["a"])(l,c,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0ceb84.b75c45ba.js.map
---
+++ prerender/js/chunk-2d22d3aa.c09b2905.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d3aa"],{f736:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("List page")])])}],r=t("2877"),u={},l=Object(r["a"])(u,a,c,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d22d3aa.c09b2905.js.map
